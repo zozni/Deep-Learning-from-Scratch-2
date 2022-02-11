04104c241512172c583e306785d99bcffde29de3
zo_zni <thekey1027@naver.com>
2022-02-11 15:17:40 +0900

    2

---
+++ Chap01-Neural_Networks_Review/100 numpy exercises/1~10.py
@@ -0,0 +1,22 @@
+# 1. Import the numpy package under the name np
+import numpy as np
+
+# 2. Print the numpy version and the configuration (넘파이 버전과 환경 확인)
+print(np.__version__)
+np.show_config()
+
+# 3. Create a null vector of size 10
+
+# 4. How to find the memory size of any array
+
+# 5. How to get the documentation of the numpy add function from the command line?
+
+# 6. Create a null vector of size 10 but the fifth value which is 1 
+
+# 7. Create a vector with values ranging from 10 to 49 
+
+# 8. Reverse a vector (first element becomes last)
+
+# 9. Create a 3x3 matrix with values ranging from 0 to 8
+
+# 10. Find indices of non-zero elements from [1,2,0,0,4,0] 
