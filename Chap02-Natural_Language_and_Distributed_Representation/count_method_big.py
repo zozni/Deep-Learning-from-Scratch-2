bbe4193163e48102e14db3406ee6da209f0bcca3
zo_zni <thekey1027@naver.com>
2022-02-19 18:03:51 +0900

    3

--- Chap02-Natural_Language_and_Distributed_Representation/count_method_big.py
+++ Chap02-Natural_Language_and_Distributed_Representation/count_method_big.py
@@ -1,4 +1,6 @@
 # chap02/count_method_big.py
+# PTB 데이터셋에 통계기반기법을 적용한 예제. 
+# 큰 행렬에 SVD를 적용해야 하므로 고속 SVD를 이용. 그러려면 sklearn 모듈을 설치해야 한다.
 import sys
 sys.path.append('..')
 import numpy as np
@@ -18,8 +20,8 @@ W = ppmi(C, verbose=True)
 
 try:
     # truncated SVD
-    from sklearn.utils.extmath import randomized_svd
-    U, S, V = randomized_svd(W, n_components=wordvec_size, n_iter=5,
+    from sklearn.utils.extmath import randomized_svd 
+    U, S, V = randomized_svd(W, n_components=wordvec_size, n_iter=5,   # 메소드 이용!
                              random_state=None)
 except:
     # SVD
@@ -30,3 +32,6 @@ word_vecs = U[:, :wordvec_size]
 querys = ['you', 'year', 'car', 'toyota']
 for query in querys:
     most_similar(query, word_to_id, id_to_word, word_vecs, top=5)
+
+
+# 단어의 의미 혹은 문법적인 관점에서 비슷한 단어들이 가까운 벡터로 나타났다. 
--- Chap02-Natural_Language_and_Distributed_Representation/show_ptb.py
+++ Chap02-Natural_Language_and_Distributed_Representation/show_ptb.py
@@ -2,9 +2,14 @@ import sys
 sys.path.append('..')
 from dataset import ptb
 
-corpus, word_to_id, id_to_word = ptb.load_data('train')
+corpus, word_to_id, id_to_word = ptb.load_data('train')  # ptb.load_data()는 데이터를 읽어들임.
+                                                         # 인수로는 train, test, valid 중 하나를 지정할 수 있다.
 
-print('말뭉치 크기: ', len(corpus))
+# corpus는 단어 ID 목록.
+# id_to_word는 단어 ID에서 단어로 변환하는 딕셔너리.
+# word_to_id는 단어에서 단어 ID로 변환하는 딕셔너리.
+
+print('말뭉치 크기: ', len(corpus))  
 print('corpus[:30]:', corpus[:30])
 print()
 print('id_to_word[0]: ', id_to_word[0])
